bec98101fcb30f1b1b538e0ed4dd17a5a60338bf
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-05-05 15:02:34 +0100

    Refactor authenticated_get function to improve session handling and token refresh logic

--- insighta/utils/request.py
+++ insighta/utils/request.py
@@ -1,12 +1,18 @@
 import requests
-from .storage import load_tokens
 import os
+from dotenv import load_dotenv
+from .storage import load_tokens
+from insighta.auth import refresh_access_token
+
+
+load_dotenv()
 
 BACKEND_URL = os.getenv("BACKEND_URL")
 
 def authenticated_get(endpoint, params=None):
     tokens = load_tokens()
 
+    # Check if logged in
     if not tokens:
         print("❌ Not logged in. Run: insighta login")
         return None
@@ -16,14 +22,29 @@ def authenticated_get(endpoint, params=None):
         "X-API-Version": "1"
     }
 
+    # Make request
     response = requests.get(
         f"{BACKEND_URL}{endpoint}",
         headers=headers,
         params=params
     )
 
+    # If access token expired, try refreshing
     if response.status_code == 401:
-        print("⚠️ Session expired. Please login again.")
-        return None
+        print("🔄 Refreshing session...")
+
+        new_tokens = refresh_access_token()
+
+        if not new_tokens:
+            return None
+
+        headers["Authorization"] = f"Bearer {new_tokens['access_token']}"
+
+        # retry request
+        response = requests.get(
+            f"{BACKEND_URL}{endpoint}",
+            headers=headers,
+            params=params
+        )
 
     return response.json()
